0a20c8030fe94a0d4178d2d3b3d02b776ee5a85c
AndradeTC86 <andrade-tc@uol.com.br>
2026-04-17 07:56:54 -0300

    Implementando thresholds

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,7 +2,11 @@ import http from 'k6/http'
 import { sleep, check } from 'k6'
 
 export const options = {
-    iterations: 10
+    iterations: 100,
+    thresholds: {
+        http_req_failed: ['rate<0.01'],
+        http_req_duration: ['p(95)<10', 'max<10']
+    }
 }
 
 export default function () {
